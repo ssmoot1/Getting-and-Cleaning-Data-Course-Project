7c71bc4ec8a83d9f34da48102f64201ddb9ef053
ssmoot1 <109162362+ssmoot1@users.noreply.github.com>
2022-08-29 13:21:43 -0400

    Create run_analyis.R

---
+++ run_analyis.R
@@ -0,0 +1,46 @@
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+#    with the average of each variable for each activity and each subject.
+
+library(reshape2)
+
+# Load activity labels + features
+activityLabels <- read.table("activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only the data on mean and standard deviation
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
+
+# Load the datasets
+train <- read.table("train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("train/Y_train.txt")
+trainSubjects <- read.table("train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("test/X_test.txt")[featuresWanted]
+testActivities <- read.table("test/Y_test.txt")
+testSubjects <- read.table("test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+# merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
+
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
+
